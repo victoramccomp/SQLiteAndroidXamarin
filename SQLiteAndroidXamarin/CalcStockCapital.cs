a3430a6f4688475540e3b5256f5d9600b2ec040f
Victor Angelo Marcorin <victor.am.ccomp@gmail.com>
2017-02-19 20:27:14 -0300

    Recovery last project To start

---
+++ SQLiteAndroidXamarin/CalcStockCapital.cs
@@ -0,0 +1,41 @@
+using System;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Widget;
+
+namespace PatrinomioAndroidXamarin
+{
+    [Activity(Label = "CalcStockCapital")]
+    public class CalcStockCapital : Activity
+    {
+        double defaultValue;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            SetContentView(Resource.Layout.CalcStockCapital);
+
+            EditText txtResult = FindViewById<EditText>(Resource.Id.txtresult);
+            ImageView imgFlag = FindViewById<ImageView>(Resource.Id.imgflag);
+            Button btnExit = FindViewById<Button>(Resource.Id.btnexit);
+
+            try
+            {
+                txtResult.Text = Intent.GetDoubleExtra("result", defaultValue).ToString();
+                imgFlag.SetImageResource(Resource.Drawable.usa);
+            }
+            catch (Exception ex)
+            {
+                Toast.MakeText(this, ex.Message, ToastLength.Short).Show();
+                throw;
+            }
+
+            btnExit.Click += delegate
+            {
+                Process.KillProcess(Process.MyPid());
+            };
+        }
+    }
+}
--- SQLiteAndroidXamarin/MainActivity.cs
+++ SQLiteAndroidXamarin/MainActivity.cs
@@ -1,18 +1,46 @@
 ﻿using Android.App;
 using Android.Widget;
 using Android.OS;
+using Android.Content;
 
-namespace SQLiteAndroidXamarin
+namespace PatrinomioAndroidXamarin
 {
-    [Activity(Label = "SQLiteAndroidXamarin", MainLauncher = true, Icon = "@drawable/icon")]
+    [Activity(MainLauncher = true, Icon = "@drawable/usaicon")]
     public class MainActivity : Activity
     {
-        protected override void OnCreate(Bundle bundle)
+        protected override void OnCreate(Bundle savedInstanceState)
         {
-            base.OnCreate(bundle);
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.Main);
 
-            // Set our view from the "main" layout resource
-            // SetContentView (Resource.Layout.Main);
+            Button btnCalc = FindViewById<Button>(Resource.Id.btncalc);
+            EditText txtIncoming = FindViewById<EditText>(Resource.Id.txtincoming);
+            EditText txtExpenditures = FindViewById<EditText>(Resource.Id.txtexpenditures);
+
+            double result, incoming, expenditures;
+
+            btnCalc.Click += delegate
+            {
+                try
+                {
+                    incoming = double.Parse(txtIncoming.Text == "" ? "0" : txtIncoming.Text);
+                    expenditures = double.Parse(txtExpenditures.Text == "" ? "0" : txtExpenditures.Text);
+                    result = expenditures - incoming;
+                    Calc(result);
+                }
+                catch (System.Exception ex)
+                {
+                    Toast.MakeText(this, ex.Message, ToastLength.Short).Show();
+                    throw;
+                }
+            };
+        }
+
+        public void Calc(double result)
+        {
+            Intent objIntent = new Intent(this, typeof(CalcStockCapital));
+            objIntent.PutExtra("result", result);
+            StartActivity(objIntent);
         }
     }
 }
